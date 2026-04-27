2928ade13b048ffb3086adfa8cca4082b952f1e4
Lucky Tech <luckytech929@gmail.com>
2026-04-27 10:24:26 +0530

    fixed

--- WMT_LabTest_Project/backend/server.js
+++ WMT_LabTest_Project/backend/server.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://testswmt.netlify.app"
+      ""
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
